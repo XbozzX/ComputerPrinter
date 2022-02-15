fa015e9c20b719bd1314aa2c57e6708c0f96a555
XbozzX <muhammad_19000649@utp.edu.my>
2022-02-15 16:23:40 +0800

    Release the code to the main branch

--- src/com/learnProgramming/Main.java
+++ src/com/learnProgramming/Main.java
@@ -3,6 +3,16 @@ package com.learnProgramming;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        PCPrinter pcPrinter1 = new PCPrinter(50,0,false,1);
+
+        pcPrinter1.StartPrint(10,2);
+        System.out.println("ID: " + pcPrinter1.getClientID());
+
+        pcPrinter1.tonerFill(20);
+        System.out.println(pcPrinter1.getTonerLevel());
+
+        System.out.println(pcPrinter1.isDuplexPrinter());
+
+        System.out.println(pcPrinter1.getNumberPagesPrinted());
     }
 }
